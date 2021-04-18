5ba1cb6780c7f957f0ed08029e21ce1085c40e11
Onur Tüfekçi <69247292+onurtufekci34@users.noreply.github.com>
2021-04-18 14:15:59 +0300

    Objects & Functions

---
+++ script.js
@@ -0,0 +1,21 @@
+let val;
+
+let num = 10;
+
+// object literal
+let yigit = {
+    name:'yigit',
+    yearOfBirth:'2010',
+    job:'student'
+}
+
+// array
+let numbers = [10,20,30];
+
+val = num;
+val = yigit;
+val = numbers;
+
+
+console.log(val);
+console.log(typeof val);
